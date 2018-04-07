b42758f0f4fbf16b7db2306c966f93e135fce5de
serkusunw <serkusunw@gmail.com>
2018-04-07 22:28:23 +0200

    Dodanie indexu głównej strony, dodanie 2 języków do wyboru. Stworzenie nawigacji.

---
+++ src/main/java/pl/serkus/controller/MainPageController.java
@@ -0,0 +1,13 @@
+package pl.serkus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainPageController {
+	
+	@RequestMapping("/")
+	public String showMainPage() {
+		return "index";
+	}
+}
